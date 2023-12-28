c63e113a38a9bda7ec06355d569f3c8b31ba6503
yusufbgdd557 <yusufbgdd557@gmail.com>
2023-12-28 22:05:12 +0300

    Frontend added to project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
 function App() {
+  const [prompt, setPrompt] = useState('');
+  const [response, setResponse] = useState('');
+  const [loading, setLoading] = useState(false);
+
+  const sendRequest = async () => {
+    try {
+      setLoading(true);
+      const result = await axios.get(`http://localhost:8080/api/v1/chat?prompt=${prompt}`);
+      setResponse(result.data);
+    } catch (error) {
+      console.error('Error during chat:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="PromptContainer">
+        <label htmlFor="prompt" className="PromptLabel">
+          Ingredients
+        </label>
+        <div className="InputButtonContainer">
+          <input
+            type="text"
+            id="prompt"
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+            className="PromptInput"
+            placeholder="Enter ingredients..."
+          />
+          <button
+            onClick={sendRequest}
+            className="PromptButton"
+            disabled={loading || !prompt.trim()}
+          >
+            {loading ? 'Loading...' : 'Get Recipe'}
+          </button>
+        </div>
+      </div>
+      {loading && (
+        <div className="LoadingContainer">
+          <p>Loading...</p>
+        </div>
+      )}
+      {response && (
+        <div className="RecipeContainer">
+          <h2 className="RecipeHeader">Recipe:</h2>
+          <p className="RecipeText">{response}</p>
+        </div>
+      )}
     </div>
   );
 }
